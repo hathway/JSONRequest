edda5e0717047fde821ba53e63ddd2cfaef9b789
Kevin Strong <kevin.strong@wearehathway.com>
2016-07-25 11:13:07 -0700

    make isConnectedToNetwork public

--- Sources/JSONRequest.swift
+++ Sources/JSONRequest.swift
@@ -208,7 +208,7 @@ public class JSONRequest {
         return String(data: data, encoding: NSUTF8StringEncoding)
     }
 
-    func isConnectedToNetwork() -> Bool {
+    public func isConnectedToNetwork() -> Bool {
         var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
