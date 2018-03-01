0123ca97536c2d9aa996f430e639b0ed22d72905
Mike Maxwell <mike@wearehathway.com>
2018-02-28 18:43:40 -0800

    Fixing forced timeout functionality, as URLSession was superseding the timeout value used on URLRequest

--- Sources/JSONRequest.swift
+++ Sources/JSONRequest.swift
@@ -149,11 +149,13 @@ open class JSONRequest {
         updateRequest(&request, headers: headers)
         updateRequest(&request, payload: payload)
 
-        let session = urlSession ?? networkSession()
+        let session = (timeOut == urlSession?.configuration.timeoutIntervalForRequest
+            ? (urlSession ?? networkSession())
+            : networkSession(forcedTimeout: timeOut))
         let start = Date()
 
         let cachedResponse: CachedURLResponse? = session.configuration.urlCache?.cachedResponse(for: request)
-        if let cache = session.configuration.urlCache, cachedResponse == nil {
+        if cachedResponse == nil {
             removeCachingHeaders(&request)
         }
 
@@ -193,9 +195,16 @@ open class JSONRequest {
     }
 
     func networkSession(forcedTimeout: TimeInterval? = nil) -> URLSession {
-        guard urlSession == nil else { return urlSession! }
-        urlSession = URLSession(configuration: JSONRequest.sessionConfig)
-        return urlSession!
+        let config = JSONRequest.sessionConfig
+        if let timeout = forcedTimeout {
+            config.timeoutIntervalForRequest = timeout
+        }
+        let session = URLSession(configuration: config)
+        if forcedTimeout == nil {
+            // if there isn't a custom timeout, set the member variable with this new session we've created for future use.
+            urlSession = session
+        }
+        return session
     }
 
     func submitSyncRequest(method: JSONRequestHttpVerb, url: String,
