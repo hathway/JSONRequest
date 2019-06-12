a12d1b506979669d6b0a4afc23e08774face1b11
Alex Antonyuk <gooddy@x-code.name>
2019-06-12 16:48:17 -0700

    Fix access

--- Sources/JSONRequestVerbs.swift
+++ Sources/JSONRequestVerbs.swift
@@ -16,7 +16,7 @@ public enum JSONRequestHttpVerb: String {
 
 // MARK: Instance basic sync/async
 
-extension JSONRequest {
+public extension JSONRequest {
     func send(method: JSONRequestHttpVerb, url: String, queryParams: JSONObject? = nil, payload: Any? = nil, headers: JSONObject? = nil, timeOut: TimeInterval? = nil) -> JSONResult {
         return submitSyncRequest(method: method, url: url, queryParams: queryParams,
                                  payload: payload, headers: headers, timeOut: timeOut)
